44d8e426865f0f7fd11c478e19e712e27cfc9e7c
Maxim Korokin <maksimkorokin1@gmail.com>
2026-02-12 14:58:38 +0200

    Add InventoryHudData

---
+++ Assets/_Game/Scripts/Items/ItemsConfig.cs
@@ -0,0 +1,36 @@
+﻿using Assets.CoreScripts;
+using System;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Items
+{
+    [CreateAssetMenu(fileName = "ItemsConfig", menuName = "ScriptableObjects/ItemsConfig")]
+    public sealed class ItemsConfig : ScriptableObject
+    {
+        [SerializeField]
+        private SpecialItemDefinition[] _specialItems;
+
+        public ItemDefinition GetSpecialItemDefinition(SpecialItemId specialItemId)
+        {
+            var index = Array.IndexOf(_specialItems, specialItemId);
+            if (index == -1)
+            {
+                SLog.Error($"Special item with id {specialItemId} is not registered.");
+                return null;
+            }
+            return _specialItems[index].ItemDefinition;
+        }
+    }
+
+    [Serializable]
+    public struct SpecialItemDefinition
+    {
+        public SpecialItemId SpecialItemId;
+        public ItemDefinition ItemDefinition;
+    }
+
+    public enum SpecialItemId
+    {
+        Gold = 10
+    }
+}
---
+++ Assets/_Game/Scripts/UI/DataAggregators/IInventoryHudData.cs
@@ -0,0 +1,67 @@
+﻿using Assets._Game.Scripts.Entities.Stats;
+using Assets._Game.Scripts.Infrastructure.Game;
+using Assets._Game.Scripts.Items;
+using Assets._Game.Scripts.Items.Inventory;
+using System;
+
+namespace Assets._Game.Scripts.UI.DataAggregators
+{
+    public interface IInventoryHudData : IDisposable
+    {
+        InventoryModel InventoryModel { get; }
+        int Gold { get; }
+        float WeightCurrent { get; }
+        float WeightMax { get; }
+
+        event Action Changed;
+    }
+
+    public class InventoryHudData : IInventoryHudData
+    {
+        private readonly InventoryModel _inventoryModel;
+        private readonly IStatsReadOnly _statsController;
+        private readonly ItemsConfig _itemsConfig;
+
+        public InventoryHudData(PlayerContext playerContext, ItemsConfig itemsConfig)
+        {
+            _inventoryModel = playerContext.IEModule.Inventory;
+            _statsController = playerContext.StatsModule.Stats;
+            _itemsConfig = itemsConfig;
+
+            _inventoryModel.Changed += OnInventoryChanged;
+        }
+
+        private void OnInventoryChanged()
+        {
+            Gold = 0;
+            foreach (var (_, snapshot) in InventoryModel.Enumerate())
+            {
+                if (snapshot == null) continue;
+
+                var itemSnapshot = snapshot.Value;
+                var goldDefinition = _itemsConfig.GetSpecialItemDefinition(SpecialItemId.Gold);
+                if (snapshot != null && itemSnapshot.Definition.Id == goldDefinition.Id)
+                {
+                    Gold += itemSnapshot.Amount;
+                }
+            }
+
+            Changed?.Invoke();
+        }
+
+        public InventoryModel InventoryModel => _inventoryModel;
+
+        public int Gold { get; private set; }
+
+        public float WeightCurrent { get; private set; }
+
+        public float WeightMax { get; private set; }
+
+        public event Action Changed;
+
+        public void Dispose()
+        {
+            _inventoryModel.Changed -= OnInventoryChanged;
+        }
+    }
+}
