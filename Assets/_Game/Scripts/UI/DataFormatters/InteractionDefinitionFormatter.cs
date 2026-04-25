7df55c8de3812bc31caf510c4ff1187774477e77
Maxim Korokin <maksimkorokin1@gmail.com>
2026-04-25 19:32:29 +0300

    Implement InteractionDefinitionFormatter

--- Assets/_Game/Scripts/UI/DataFormatters/InteractionDefinitionFormatter.cs
+++ Assets/_Game/Scripts/UI/DataFormatters/InteractionDefinitionFormatter.cs
@@ -1,4 +1,6 @@
 ﻿using Assets._Game.Scripts.Entities.Interactions;
+using Assets._Game.Scripts.Shared.Extensions;
+using System.Text;
 
 namespace Assets._Game.Scripts.UI.DataFormatters
 {
@@ -6,7 +8,61 @@ namespace Assets._Game.Scripts.UI.DataFormatters
     {
         public string FormatData(InteractionDefinition data)
         {
-            return "Applies interaction";
+            if (data == null || data.Root == null)
+                return string.Empty;
+
+            var stringBuilder = new StringBuilder();
+            FormatStep(data.Root, stringBuilder);
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        private void FormatStep(InteractionDefinition.StepData step, StringBuilder stringBuilder)
+        {
+            if (step == null)
+                return;
+
+            switch (step)
+            {
+                case InteractionDefinition.StepsStepData stepsData:
+                    foreach (var subStep in stepsData.Steps)
+                    {
+                        FormatStep(subStep, stringBuilder);
+                    }
+                    break;
+
+                case InteractionDefinition.DamageData damageData:
+                    var damageTypeText = damageData.Spec.Type.GetDescription();
+                    var flatText = damageData.Spec.Flat > 0 ? $"{(int)damageData.Spec.Flat}" : "";
+                    var scaleText = damageData.Spec.AttackScale > 0 ? $"{damageData.Spec.AttackScale * 100:0.##}% ATK" : "";
+
+                    var damageValue = "";
+                    if (damageData.Spec.Flat > 0 && damageData.Spec.AttackScale > 0)
+                        damageValue = $"{flatText} + {scaleText}";
+                    else if (damageData.Spec.Flat > 0)
+                        damageValue = $"{flatText}";
+                    else if (damageData.Spec.AttackScale > 0)
+                        damageValue = $"{scaleText}";
+
+                    var critText = damageData.Spec.CanCrit ? "(can crit)" : "";
+                    stringBuilder.AppendLine($"Damage: {damageValue} {damageTypeText} {critText}");
+                    break;
+
+                case InteractionDefinition.HealData healData:
+                    stringBuilder.AppendLine($"Heal: {healData.Amount}");
+                    break;
+
+                case InteractionDefinition.OverrideControlStepData controlData:
+                    var controlType = controlData.ControlProvider?.GetType().Name.Replace("ControlProviderData", "") ?? "None";
+                    stringBuilder.AppendLine($"Override Control: {controlType}");
+                    break;
+
+                case InteractionDefinition.ReviveStepData:
+                    stringBuilder.AppendLine($"Revive");
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
