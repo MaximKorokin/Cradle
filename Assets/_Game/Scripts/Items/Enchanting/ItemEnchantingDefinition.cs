0ccf10375f651212092f83a5e68f9ba454ccbf9b
Maxim Korokin <maksimkorokin1@gmail.com>
2026-09-01 02:10:03 +0300

    Add EnchantingDefinition and example for BloodStone

---
+++ Assets/_Game/Scripts/Items/Enchanting/ItemEnchantingDefinition.cs
@@ -0,0 +1,68 @@
+﻿using Assets._Game.Scripts.Entities.Stats;
+using Assets._Game.Scripts.Shared;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Items.Enchanting
+{
+    [CreateAssetMenu(menuName = "Definitions/ItemEnchanting", fileName = "Enchanting")]
+    public class ItemEnchantingDefinition : GuidScriptableObject
+    {
+        [SerializeField]
+        private EnchantLevelDefinition[] _levels;
+
+        [SerializeField]
+        private EnchantMethodDefinition[] _methods;
+
+        public IReadOnlyList<EnchantLevelDefinition> Levels => _levels;
+        public IReadOnlyList<EnchantMethodDefinition> Methods => _methods;
+    }
+
+    [Serializable]
+    public class EnchantLevelDefinition
+    {
+        [SerializeField]
+        private StatModifier[] _bonuses;
+
+        public IReadOnlyList<StatModifier> Bonuses => _bonuses;
+    }
+
+    [Serializable]
+    public class EnchantMethodDefinition
+    {
+        [field: SerializeField]
+        public ItemDefinition Item { get; private set; }
+
+        [SerializeField]
+        private EnchantLevelRuleDefinition[] _rules;
+
+        public IReadOnlyList<EnchantLevelRuleDefinition> Rules => _rules;
+    }
+
+    [Serializable]
+    public class EnchantLevelRuleDefinition
+    {
+        [field: SerializeField]
+        [field: Range(0f, 1f)]
+        public float SuccessChance { get; private set; }
+
+        [field: SerializeField]
+        public EnchantFailureDefinition Failure { get; private set; }
+    }
+
+    [Serializable]
+    public class EnchantFailureDefinition
+    {
+        [field: SerializeField]
+        public EnchantFailureType Type { get; private set; }
+    }
+
+    public enum EnchantFailureType
+    {
+        None,
+        Downgrade,
+        Reset,
+        Destroy
+    }
+}
---
+++ Assets/_Game/Scripts/Items/Traits/EnchantableTrait.cs
@@ -0,0 +1,13 @@
+﻿using Assets._Game.Scripts.Items.Enchanting;
+using System;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Items.Traits
+{
+    [Serializable]
+    public class EnchantableTrait : ItemTraitBase
+    {
+        [field: SerializeField]
+        public ItemEnchantingDefinition ItemEnchantingDefinition { get; private set; }
+    }
+}
